11ddb1f69f1de94737bf40d4484a6fa3959d1f51
David Adams <macgyverda@gmail.com>
2017-12-16 23:17:04 -0800

    Initial commit for test, doesn't do much!

---
+++ test.py
@@ -0,0 +1,13 @@
+print('Simple calculation test')
+inputNum = input('Enter Number: ')
+inputOp = input('Enter Operation: ')
+inputNum2 = input('Enter Second Number: ')
+if inputOp == '+':
+    print(float(inputNum) + float(inputNum2))
+if inputOp == '-':
+    print(float(inputNum) - float(inputNum2))
+if inputOp == '*':
+    print(float(inputNum) * float(inputNum2))
+if inputOp == '/':
+    print(float(inputNum) / float(inputNum2))        
+    
