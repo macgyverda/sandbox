cee8b30c922fc2c12990b23b0fb5d77d35c45e04
David Adams <macgyverda@gmail.com>
2017-12-16 23:28:28 -0800

    Testing a branched feature

---
+++ test2.py
@@ -0,0 +1,14 @@
+% Adding comments maybe this would do something else
+print('Simple calculation test')
+inputNum = input('Enter Number: ')
+inputOp = input('Enter Operation: ')
+inputNum2 = input('Enter Second Number: ')
+if inputOp == '+':
+    print(float(inputNum) + float(inputNum2))
+if inputOp == '-':
+    print(float(inputNum) - float(inputNum2))
+if inputOp == '*':
+    print(float(inputNum) * float(inputNum2))
+if inputOp == '/':
+    print(float(inputNum) / float(inputNum2))        
+    
